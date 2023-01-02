e52c883947c39d322f8ba2076d773905ba10fe32
Amarnath <motorolaonefusion@gmail.com>
2023-01-02 16:48:31 +0530

    Update background

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import pygame
 
-WIDTH , HEIGHT = 900 , 500
+WIDTH , HEIGHT = 1000 , 650
 
 WIN  = pygame.display.set_mode((WIDTH , HEIGHT))
+pygame.display.set_caption('War Zone')
+
+WHITE = (255,255,255)
+
+FPS = 60
+
+def draw_window():
+    WIN.fill(WHITE)
+    pygame.display.update()
+
+
 
 
 def main():
-    
+    clock = pygame.time.Clock()
     run = True
-
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        draw_window()
+
 
     pygame.quit()
 
