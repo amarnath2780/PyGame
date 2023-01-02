d6d961d83e71135b2699d207f454bffd6e3f74d1
Amarnath <motorolaonefusion@gmail.com>
2023-01-02 23:45:08 +0530

    moment added for yellow

--- main.py
+++ main.py
@@ -1,22 +1,50 @@
 import pygame
+import os
 
-WIDTH , HEIGHT = 1000 , 650
+WIDTH , HEIGHT = 900 , 650
 
 WIN  = pygame.display.set_mode((WIDTH , HEIGHT))
 pygame.display.set_caption('War Zone')
 
 WHITE = (255,255,255)
+SPACESHIP_WIDTH  , SPACESHIP_HEIGHT = 55 , 40
 
 FPS = 60
 
-def draw_window():
+VEL = 5
+
+
+def yellow_handle_movement(key_pressed, yellow):
+    if key_pressed[pygame.K_a]:
+        yellow.x -= VEL
+    if key_pressed[pygame.K_d]:
+        yellow.x += VEL
+    if key_pressed[pygame.K_w]:
+        yellow.y -= VEL
+    if key_pressed[pygame.K_s]:
+        yellow.y += VEL
+
+
+def draw_window(red,yellow):
     WIN.fill(WHITE)
+    WIN.blit(RED_SPACESHIP , (red.x , red.y))
+    WIN.blit(YELLOW_SPACESHIP,(yellow.x , yellow.y))
     pygame.display.update()
 
-
-
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.rotate(
+    pygame.transform.scale(YELLOW_SPACESHIP_IMAGE , (SPACESHIP_WIDTH,SPACESHIP_HEIGHT)) , 90
+)
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
+RED_SPACESHIP = pygame.transform.rotate(
+    pygame.transform.scale(RED_SPACESHIP_IMAGE , (SPACESHIP_WIDTH,SPACESHIP_HEIGHT)) , 270
+)
 
 def main():
+
+    yellow  = pygame.Rect(100,300 , SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    red = pygame.Rect(700,300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -25,8 +53,13 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        draw_window()
+        key_pressed = pygame.key.get_pressed()
+        yellow_handle_movement(key_pressed , yellow)
+        
 
+        draw_window(red, yellow)
+
+        
 
     pygame.quit()
 
